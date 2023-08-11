325ece4eb7f3dc2d8e860d47a35384aac51b83c3
sundaywixy <sundayemmanuelng@aol.com>
2023-08-11 06:24:37 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - this is a function in c
+  * Return: returns zero
+  */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
