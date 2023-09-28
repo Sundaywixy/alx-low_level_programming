28296e21fd7064f295dec9a764ed0fc88bf0e7a2
sundaywixy <sundayemmanuelng@aol.com>
2023-09-29 00:59:00 +0100

    100 pushed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,16 @@
-Hol^D
+nclude "main.h"
+
+/**
+ * get_endianness -Machine is checked if its  little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
+
+
+
